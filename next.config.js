e0db8a67202fbfc7e1ccb456dcbd97b9dc3c0839
David Corbacho <david.corbacho@wunder.io>
2021-05-28 08:00:10 +0300

    Disable powered by header

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// All configuration options and default values:
+// https://github.com/vercel/next.js/blob/master/packages/next/next-server/server/config-shared.ts
+
+const nextConfig = {
+  poweredByHeader: false, // for security
+};
+
+module.exports = nextConfig;
