45b796130aedb9f84e9a81e34dfbfe2f36b41fb9
Oskari Heinonen <osheinonen@gmail.com>
2021-07-08 14:43:24 +0300

    Added jest and babel configs, updated tsconfig to support path aliases

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  moduleNameMapper: {
+    "\\.(scss|sass|css)$": "identity-obj-proxy",
+    "@/components/(.*)": "<rootDir>/src/components/$1",
+  },
+};
