dc848d6278b5006b052b9a4094b330df0e728ac9
Oskari Heinonen <osheinonen@gmail.com>
2021-09-03 14:08:03 +0300

    Fail

--- src/components/welcome/welcome.tsx
+++ src/components/welcome/welcome.tsx
@@ -5,8 +5,8 @@ export interface Properties {
 function Welcome({ showCarrot }: Properties): JSX.Element {
   return (
     <p>
-      Welcome to Next.js!
-      {showCarrot && " 🥕"}
+      Welcome to Next.js! askldj
+      {showCarrot && " 🥕asdfkljsadfhjk"}
     </p>
   );
 }
